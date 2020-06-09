c3f39e5d4a5281b65da54e9e91d468541398d214
moxwel <49621788+moxwel@users.noreply.github.com>
2020-06-09 17:06:28 -0400

    List Array TDA

---
+++ C/11-edd/1-listas/1-list_example.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "./list_array_TDA.c"
+
+int main() {
+    tLista mi_lista;
+    tLista *pl = &mi_lista;
+
+    printf("1-Inicializando lista...\n");
+    initList(pl);
+    printList(pl);
+
+    printf("2-Agregando elementos...\n");
+    append(pl, 54);
+    append(pl, 4);
+    append(pl, 3214);
+    append(pl, 24);
+    append(pl, 100);
+    append(pl, 8367);
+    printList(pl);
+
+    printf("3-Moviendo cursor a la posicion 4...\n");
+    moveToPos(pl, 4);
+    printList(pl);
+
+    printf("4-Insertando elemento en la posicion 4...\n");
+    insert(pl,9876);
+    printList(pl);
+
+    printf("5-Moviendo cursor a la posicion 1...\n");
+    moveToPos(pl, 1);
+    printList(pl);
+
+    printf("6-Eliminando elemento...\n");
+    erase(pl);
+    printList(pl);
+
+    deleteList(pl);
+    return 0;
+}
--- C/11-edd/1-listas/list_array_TDA.c
+++ C/11-edd/1-listas/list_array_TDA.c
@@ -1,32 +1,41 @@
-#include <stdio.h>
-#include <stdlib.h>
+// Este archivo puede incluirse de forma individual en cualquier
+// programa en el que se necesiten listas.
 
 // Como los elementos se van a guardar en un arreglo, significa que tienen un tamaño maximo
-
 // Suponemos que el tamaño maximo de la lista es 'MAX_SIZE'
 #define MAX_SIZE 1000000
 
-// ==========
+
+
+// ====================
 // CREACION DEL TDA
+// ====================
 
 // Vamos a suponer que los elementos de la lista van a ser 'ints'
 typedef int tElemLista;
 
 // Se define la estructura "Lista", en este caso, basada en un arreglo
 typedef struct {
+
     unsigned int maxSize; // El tamaño maximo del arreglo
+
     unsigned int listSize; // EL tamaño de la "porcion del arreglo" que se esta utilizando
+
     unsigned int curr; // El cursor que denota la posicion actual
+
     tElemLista* listArray; // El arreglo en donde se van a guardar los elementos
 
     // Unsigned significa "sin signo", no hay negativos. Tiene logica ya que los indices
     // de los arreglos son desde el 0 en adelante.
 } tLista;
 
-// ==========
-// CREACION DE INTERFAZ
 
-// Inicializador de lista
+
+// ====================
+// CREACION DE INTERFAZ
+// ====================
+
+// Inicializador de lista. Asigna memoria.
 void initList(tLista *L) {
     L->maxSize = MAX_SIZE;
     L->listSize = 0;
@@ -34,33 +43,16 @@ void initList(tLista *L) {
     L->listArray = (tElemLista*)malloc(L->maxSize * sizeof(tElemLista));
 }
 
-// Limpiar lista
-void clearList(tLista *L) {
-    free(L->listArray);
-    L->listSize = 0;
-    L->curr = 0;
-    L->listArray = (tElemLista*)malloc(L->maxSize * sizeof(tElemLista));
-}
-
-// Borrar lista
+// Borrar lista. Libera memoria.
 void deleteList(tLista *L) {
     free(L->listArray);
 }
 
-// Agregar un elemento al final de la lista (retorna la cantidad de elementos insertados)
-int append(tLista *L, tElemLista item) {
-    // Si el tamaño de la lista ya es el maximo posible, entonces no agrega nada
-    if (L->listSize >= L->maxSize) {
-        return 0;
-    }
 
-    L->listArray[L->listSize] = item;
-    L->listSize++;
-    return 1;
-}
 
 // Insertar elemento en la ubicacion del cursor (retorna la cantidad de elementos insertados)
 int insert(tLista *L, tElemLista item) {
+    // Si el tamaño de la lista ya es el maximo posible, entonces no agrega nada
     if (L->listSize >= L->maxSize) {
         return 0;
     }
@@ -74,7 +66,19 @@ int insert(tLista *L, tElemLista item) {
     // Al final, la casilla en donde esta el cursor, se podra insertar el elemento deseado
     L->listArray[L->curr] = item;
     L->listSize++;
+    return 1;
+}
 
+// Agregar un elemento al final de la lista (retorna la cantidad de elementos insertados)
+int append(tLista *L, tElemLista item) {
+    // Si el tamaño de la lista ya es el maximo posible, entonces no agrega nada
+    if (L->listSize >= L->maxSize) {
+        return 0;
+    }
+
+    // Agrega un elemento al final de la lista.
+    L->listArray[L->listSize] = item;
+    L->listSize++;
     return 1;
 }
 
@@ -83,16 +87,17 @@ tElemLista erase(tLista *L) {
     // Se guarda el elemento actual para despues retornarlo
     tElemLista item = L->listArray[L->curr];
 
-    // Si el cursor esta en la ultima posicion de la lista, entonces se mueve un puesto mas atras
-    if (L->curr == (L->listSize - 1)) {
+    // Si el cursor esta en la ultima posicion de la lista y
+    // ademas no es el unico elemento, entonces se mueve un puesto mas atras
+    if (L->curr == (L->listSize - 1) && L->listSize > 1) {
         L->curr--;
         L->listSize--;
         return item;
     }
 
-    // Hay que mover todos los elementos despues del cursor una casilla mas a la izquierda
+    // Sino, hay que mover todos los elementos despues del cursor una casilla mas a la izquierda
     int i = 0;
-    for (i = L->curr; i < L->curr; i++) {
+    for (i = L->curr; i < (L->listSize - 1); i++) {
         L->listArray[i] = L->listArray[i+1];
     }
 
@@ -101,9 +106,25 @@ tElemLista erase(tLista *L) {
     return item;
 }
 
+// Limpiar lista
+void clear(tLista *L) {
+    //Liberar y volver a declarar
+    free(L->listArray);
+    L->listSize = 0;
+    L->curr = 0;
+    L->listArray = (tElemLista*)malloc(L->maxSize * sizeof(tElemLista));
+}
+
 // Retorna el tamaño de la lista
 int length(tLista *L) {
     return L->listSize;
+}
+
+
+
+// Retorna la ubicacion del cursor
+int currPos(tLista *L) {
+    return L->curr;
 }
 
 // Retorna el valor del cursor actual
@@ -111,24 +132,9 @@ tElemLista getValue(tLista *L) {
     return L->listArray[L->curr];
 }
 
-// Retorna la ubicacion del cursor
-int currPos(tLista *L) {
-    return L->curr;
-}
-
-// Mover el cursor a la posicion indicada (1 = exito)
-int moveToPos(tLista *L, int pos) {
-    if (pos >= L->listSize) {
-        printf("Error\n");
-        return 0;
-    }
-
-    L->curr = pos;
-    return 1;
-}
-
 // Mover el cursor una casilla mas adelante
 void next(tLista *L) {
+    // Solo lo mueve si el cursor no esta al final de la lista
     if (L->curr < (L->listSize - 1)) {
         L->curr++;
     }
@@ -136,9 +142,21 @@ void next(tLista *L) {
 
 // Mover el cursor una casilla mas atras
 void prev(tLista *L) {
+    // Solo lo mueve si el cursor no esta al inicio de la lista
     if (L->curr > 0) {
         L->curr--;
     }
+}
+
+// Mover el cursor a la posicion indicada (1 = exito)
+int moveToPos(tLista *L, int pos) {
+    // Asegurarse que la posicion a la que se quiere mover no excede el tamaño de la lista
+    if (pos >= L->listSize) {
+        return 0;
+    }
+
+    L->curr = pos;
+    return 1;
 }
 
 // Mover el cursor hacia el inicio de la lista
@@ -148,33 +166,23 @@ void moveToStart(tLista *L) {
 
 // Mover el cursor hacia el final de la lista
 void moveToEnd(tLista *L) {
-    L->curr = L->listSize-1;
+    L->curr = (L->listSize - 1);
 }
+
+
 
 // Imprimir lista
-void printl(tLista *L) {
-    printf("Pos:\tValor:\tTam:%i\tCur:%i\n",length(L),currPos(L));
+void printList(tLista *L) {
+    printf("\n==========::LISTA::==========\n");
+    printf("Pos:\tVal:\tLen:%i\tCur:%i\n",length(L),currPos(L));
+
     for (int i = 0; i < length(L); i++) {
-        printf("%i\t%i\n",i,L->listArray[i]);
+        if (i == currPos(L)) {
+            printf(">%i\t%i\n",i,L->listArray[i]);
+        } else {
+            printf("%i\t%i\n",i,L->listArray[i]);
+        }
     }
-}
 
-// ==========
-
-int main() {
-    tLista mi_lista;
-    tLista *pl = &mi_lista;
-    initList(pl);
-
-    // Agregar elementos
-    append(pl, 54);
-    append(pl, 4);
-    append(pl, 3214);
-    append(pl, 24);
-    append(pl, 100);
-
-    printl(pl);
-
-    deleteList(pl);
-    return 0;
+    printf("=============================\n\n");
 }
