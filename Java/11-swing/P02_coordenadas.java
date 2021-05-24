ba965f990db7ac8f3f3b564d8eece780150394ed
moxwel <49621788+moxwel@users.noreply.github.com>
2021-05-24 18:26:25 -0400

    swing2

---
+++ Java/11-swing/P02_coordenadas.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class P02_coordenadas extends JFrame {
+    public P02_coordenadas() {
+        setLayout(null);
+        int wea[] = new int[5];
+
+        int num = wea.length;
+
+        System.out.println(num);
+
+    }
+}
