4bc32846088eeb0fd1676d31b5d0a931e2271bb0
moxwel <49621788+moxwel@users.noreply.github.com>
2021-05-28 02:46:18 -0400

    herencia2

---
+++ Java/02-OOP/P13_herencia2.java
@@ -0,0 +1,39 @@
+/*
+- Los constructores no se heredan, pero se ejecutan cuando una subclase se instancia.
+
+- Una subclase puede tener variables con el mismo nombre que las variables de su clase padre.
+  Se puede acceder a la variable de la clase padre con la palabra 'super'.
+*/
+
+class MiClase {
+    // Notese la variable 'mi_variable' de 'MiClase' y 'MiOtraClase'
+    int mi_variable = 56;
+
+    MiClase() {
+        System.out.println("Se ha creado un nuevo objeto 'MiClase'.");
+    }
+}
+
+class OtraClase extends MiClase {
+    // Para 'OtraClase', el valor de 'mi_variable' sera 12
+    // Pero para 'MiClase' el valor sera de 56.
+    int mi_variable = 12;
+
+    // Cuando se declare un objeto de clase 'OtraClase', se va a ejecutar primero el
+    // constructor de 'MiClase'
+    OtraClase() {
+        System.out.println("Se ha creado un nuevo objeto 'OtraClase'.");
+
+        System.out.println("Contenido de 'mi_variable' local (OtraClase): " + mi_variable);
+        // 12 (variable local de la clase)
+
+        System.out.println("Contenido de 'mi_variable' de la clase padre (MiClase): " + super.mi_variable);
+        // 56 (variable de la superclase)
+    }
+}
+
+public class P13_herencia2 {
+    public static void main(String[] args) {
+        OtraClase a = new OtraClase();
+    }
+}
