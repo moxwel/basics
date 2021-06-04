6af29d1af22baf240722c5e0d02e45ef22642c4d
moxwel <49621788+moxwel@users.noreply.github.com>
2021-06-04 00:45:13 -0400

    this

---
+++ Java/02-OOP/P18_this.java
@@ -0,0 +1,32 @@
+// La palabra clave 'this' nos sirve para poder referenciar a la instancia actual de la clase.
+// Viene util cuando hay ocultamiento de nombres por parte de parametros en los metodos, o
+// sobreescritura de metodos.
+
+
+class MiClase {
+    int dato;
+
+    MiClase (int dato) {
+        // Aqui debemos usar 'this' ya que dentro del constructor, decir 'dato' en realidad se
+        // refiere al parametro, y no al miembro 'dato' de la clase.
+
+        // Utilizando 'this.dato', nos estamos refiriendo al miembro 'dato' de la instancia de
+        // la clase actual
+        this.dato = dato;
+    }
+
+    public void unMetodo() {
+        System.out.println("Funcion de la clase 'MiClase'. Valor guardado: " + dato);
+    }
+
+}
+
+
+public class P18_this {
+    public static void main(String[] args) {
+        MiClase objeto = new MiClase(34);
+
+        objeto.unMetodo();
+
+    }
+}
