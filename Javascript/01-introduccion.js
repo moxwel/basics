f6fbe121f656a886015a6123c97cc02c1ccd0c7c
moxwel <49621788+moxwel@users.noreply.github.com>
2022-10-16 22:11:07 -0300

    js tutorial inicial

---
+++ Javascript/01-introduccion.js
@@ -0,0 +1,22 @@
+// Javascript es un lenguaje de programacion interpretado originalmente diseñado para
+// dar dinamismo a las paginas web; pero actualmente su uso se ha ampliado exponencialmente
+// desde aplicaciones web, servidor, aplicaciones de escritorio, aplicaciones moviles, etc.
+
+// Javascript se puede ejecutar desde el navagador, desde algun archivo HTML, o desde Node.js.
+
+// Para imprimir elementos en consola:
+
+console.log('Hola consola.')
+
+// En un documento HTML, codigo en Javascript debe estar dentro de la etiqueta <script>,
+// alli podemos escribir dentro del documento HTML:
+
+document.write('Hola pagina web.')
+
+// Esto escribirá texto en el HTML, no en la consola. (No funciona en Node, pues se necesita un
+// navegador)
+
+/*
+Para escribir comentarios multilinea, se hace con
+esta sintaxis.
+*/
