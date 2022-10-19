02b3f242a8fd00e6ca4705a3702a983b15412b6f
moxwel <49621788+moxwel@users.noreply.github.com>
2022-10-19 01:21:07 -0300

    formatting

---
+++ Javascript/02.2-formateo.js
@@ -0,0 +1,14 @@
+var nombre = "Max"
+var color = "morado"
+
+// Un string puede ser formateado de diferentes formas.
+
+// Concatenando strings
+var mensaje1 = "Hola soy " + nombre + " y mi color favorito es " + color + ".";
+
+// Usando placeholders
+var mensaje2 = `Hola soy ${nombre} y mi color favorito es ${color}.`;
+
+console.log(mensaje1) // Hola soy Max y mi color favorito es morado.
+
+console.log(mensaje2) // Hola soy Max y mi color favorito es morado.
